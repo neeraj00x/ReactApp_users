540088344628ebf3f8daa32da1592b492cddf909
Neeraj Pratap Singh <nprataps@iitk.ac.in>
2022-05-28 04:20:58 +0530

    updated

--- src/components/content.js
+++ src/components/content.js
@@ -11,11 +11,11 @@ function Content() {
     const [data] = useFetch("https://jsonplaceholder.typicode.com/users");
 
     if (!data) return <div className="row">
-        <h1> Pleses wait some time.... </h1> </div>;
+        <h1> Please wait some time... </h1> </div>;
 
     return (
         <div className="Content">
-            {data &&
+            {
                 data.map((item) => (
                     <Rows item = {item}/>
                 ))
